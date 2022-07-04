88a785e011cdc81665964df53cf40deeab2f9963
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-07-04 17:04:50 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -23,13 +23,9 @@ if (($result_anime) and ($result_anime->rowCount() != 0)){
 		$dados .= "<div class='thumb_div col-xxl-3'>";
 		$dados .= "<a href='anime_detalhes.php?id_anime=$id_anime' title='Detalhes do Anime' target='_blank'>";
 		$dados .= "<img class='thumb_img' src='imgs/anime/$img_mini'>";
-		$dados .= "<div class='col-xxl-10'>";
-		$dados .= "<span class='span_nome'>$nome_anime</span>";
-		$dados .= "</div>";
-		$dados .= "</a>";
-		$dados .= "</div>";
+		$dados .= "<div class='col-xxl-10'><span class='span_nome'>$nome_anime</span>";
+		$dados .= "</div></a></div>";
 	}
-	
 	$dados .= "</div>";
 	
 	//Paginação - somar a quantidade de registro que ha no BD
@@ -42,8 +38,8 @@ if (($result_anime) and ($result_anime->rowCount() != 0)){
 	$quantidade_pg = ceil($row_pg['num_result']/$qnt_result_pg);// foi usado o CEIL para o arendondamento do numero
 	$max_links = 5; 
 	
-	$dados .= "<nav class='menu_pag' aria-label='Page navigation example'>";
-	$dados .= "<ul class='menu_pag pagination pagination-sm justify-content-center'>";
+	$dados .= "<br><nav class='menu_pag' aria-label='Barra de navegação'><ul class='menu_pag pagination pagination-sm justify-content-center'>";
+	
 	// pagina anterior
 	$dados .= "<li class='menu_pag page-item'><a class='page-link' href='#' onclick='listarAnimes(1)'>Primeira</a></li>";
 	for($pag_ant = $pagina - $max_links; $pag_ant <= $pagina - 1; $pag_ant++) {
@@ -53,21 +49,15 @@ if (($result_anime) and ($result_anime->rowCount() != 0)){
 	}
 	// pagina ATIVA
 	$dados .= "<li class='menu_pag page-item active' aria-current='page'>";
-    $dados .= "<a class='page-link' href='#'>$pagina</a>";
-    $dados .= "</li>";
+    $dados .= "<a class='page-link' href='#'>$pagina</a></li>";
 		
 	for($pag_dep = $pagina + 1; $pag_dep <= $pagina + $max_links; $pag_dep++){
 		if($pag_dep <= $quantidade_pg){
 			$dados .= "<li class='menu_pag page-item'><a class='page-link' href='#' onclick='listarAnimes($pag_dep)'>$pag_dep</a></li>";
 		}
 	}
-	
 	// ir para ultima pagina
-	$dados .= "<li class='menu_pag page-item'><a class='page-link' href='#' onclick='listarAnimes($quantidade_pg)'>Ultima</a></li>";
-	
-	$dados .= "</ul>";
-	$dados .= "</nav>";
-	
+	$dados .= "<li class='menu_pag page-item'><a class='page-link' href='#' onclick='listarAnimes($quantidade_pg)'>Ultima</a></li></ul></nav>";
 	// retorna os dados da função JS
 	$retorna = ['status' => true, 'dados' => $dados, 'inicio' => $inicio];
 } else {
@@ -76,6 +66,5 @@ if (($result_anime) and ($result_anime->rowCount() != 0)){
 }else {
 	$retorna = ['status' => false, 'msg' => "<p style='color: #f00;'>Erro: Nenhun Anime Encontrado2!</p>"];
 }
-	
 echo json_encode($retorna);
 ?>
