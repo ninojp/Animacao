65094cae06e326045ccfb6322fbddbd9b1eec63a
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-07-07 15:02:57 -0300

    criação do listar séries

--- series.php
+++ series.php
@@ -3,7 +3,7 @@ session_start(); //deve ser a primeira linha de codigo da pagina(DICA), antes me
 //Limpar o buffer de saida
 ob_start();
 // conecta com o banco de dados
-include_once('conecta.php');
+// include_once('conecta.php');
 include_once('consulta_count.php');
 //CONSULTA na tabela ANIME JOIN IMAGEM ordenado por nome_anime
 // $consulta_anime = $conecta->query("SELECT * FROM anime AS a LEFT JOIN imagem AS img ON a.id_anime = img.anime_id_anime ORDER BY nome_anime ASC");
@@ -15,7 +15,7 @@ include_once('consulta_count.php');
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="animação, anime, animação 3d, filmes anime, ecchi, desenhos animados">
-	<title>Animação</title>
+	<title>Séries</title>
 	<!-- BOOTSTRAP CSS-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<!-- ICONs google Fonts  -->
@@ -27,12 +27,6 @@ include_once('consulta_count.php');
 	<link rel="stylesheet" type="text/css" href="css/geral_style.css">
 </head>
 <body>
-	<!--	DIV VIDEO video_dragao (loop) FOI RETIRADO pois estava consumindo muita MEMÓRIA 
-<div class="video_dragao">
-	<video playsinline autoplay  muted>
-		<source src="video/dragon_black_proto.webm" type="video/webm">
-	</video>
-</div> -->
 <?php
 	include_once 'navbar_top.php';
 	include_once 'header.php';
@@ -75,12 +69,13 @@ include_once('consulta_count.php');
 						</div>
 					</div>
 				</div>
-				<!-- Bloco de codigo de inserção do LISTAR.PHP -->
+				<!-- Exibir MENSAGENs nesta parte -  msgAlertSerie  -->
 				<div class="row text-center">
-					<span id="msgAlerta"></span>
+					<span id="msgAlertSerie"></span>
 				</div>
+				<!-- Tentativa de listar as SERIES aqui - listar_series -->
 				<div class="row text-center">
-					<span class="listar-animes"></span>
+					<span class="listar_series"></span>
 				</div>
 			</div>
 	<?php
@@ -90,11 +85,12 @@ include_once('consulta_count.php');
 	</main>
 	<?php
 	include_once('rodape.php');
+	include_once('banner_girls.php');
 	?>
 <!-- BOOTSTRAP JQUERRY -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- meu arquivo JavaScript Custom.js para o arquivo listar-animes -->
-<script src="js/custom.js"></script>
+<script src="js/series.js"></script>
 <!-- <script src="js/categorias.js"></script> -->
 </body>
 </html>
--- side_bar.php
+++ side_bar.php
@@ -5,10 +5,10 @@ include_once('consulta_count.php');
 <div class="col-xxl-2 col-xl-2 col-lg-2 text-center">
 	<div class="row">
 	<div class="col-xx-12" style="margin-top: 3rem;">
-			<h5>Animes (<?php echo $exibe_count_anime['qnt_anime'];?>)</h5>
+			<h5><a href="index.php" target="_blank" alt="Página da Lista dos Animes">Animes</a> (<?php echo $exibe_count_anime['qnt_anime'];?>)</h5>
 		</div>
 		<div class="col-xx-12" style="margin-top: 1rem;">
-			<h5>Séries (<?php echo $exibe_count_serie['qnt_serie'];?>)</h5>
+			<h5><a href="series.php" target="_blank" alt="Página da Lista das Séries">Séries</a> (<?php echo $exibe_count_serie['qnt_serie'];?>)</h5>
 		</div>
 		<div class="col-xx-12" style="margin-top: 1rem;">
 			<h5>Filmes (<?php echo $exibe_count_filme['qnt_filme'];?>)</h5>
